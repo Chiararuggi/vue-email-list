ad49655b56155b133b69025f1b428b6410a3de13
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-20 17:56:35 +0100

    generate email x10

--- js/main.js
+++ js/main.js
@@ -3,38 +3,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            userEmail: [
-                {
-                    email: "",
-                },
-                {
-                    email: "",
-                },
-                {
-                    email: "",
-                },
-                {
-                    email: "",
-                },
-                {
-                    email: "",
-                },
-                {
-                    email: "",
-                },
-                {
-                    email: "",
-                },
-                {
-                    email: "",
-                },
-                {
-                    email: "",
-                },
-                {
-                    email: "",
-                },
-            ],
+            userEmail: [],
         }
     },
     methods: {
@@ -45,12 +14,10 @@ createApp({
     mounted() {
         //generate email and put it into a variable
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            if(response.data.success) {
-                this.userEmail.email = response.data.response;
-                console.log(this.userEmail.email);
-                console.log(this.userEmail);
+            for (let i = 0; i < 10; i++) {
+                this.userEmail[i] = response.data.response;
+                console.log(this.userEmail); 
             }
         });
-        
     }
 }).mount('#app')
