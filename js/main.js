7c8056ab7b6f3061a0ff9df776d7d4003796c429
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-20 17:33:31 +0100

    print random email on console log

--- js/main.js
+++ js/main.js
@@ -1,15 +1,56 @@
-const { createApp } = Vue;
+const { createApp } = Vue
 
-const opzioni = {
-  data: function () {
-    return {
+createApp({
+    data() {
+        return {
+            userEmail: [
+                {
+                    email: "",
+                },
+                {
+                    email: "",
+                },
+                {
+                    email: "",
+                },
+                {
+                    email: "",
+                },
+                {
+                    email: "",
+                },
+                {
+                    email: "",
+                },
+                {
+                    email: "",
+                },
+                {
+                    email: "",
+                },
+                {
+                    email: "",
+                },
+                {
+                    email: "",
+                },
+            ],
+        }
+    },
+    methods: {
+        generateEmail(){
 
-    };
-  },
-
-  methods: {
-
-  },
-};
-
-createApp(opzioni).mount("#app");
+        },
+    },
+    mounted() {
+        //generate email and put it into a variable
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
+            if(response.data.success) {
+                this.userEmail.email = response.data.response;
+                console.log(this.userEmail.email);
+                console.log(this.userEmail);
+            }
+        });
+        
+    }
+}).mount('#app')
