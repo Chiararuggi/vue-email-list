1554d084272cfa53c77146189febe11a119b3944
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-20 19:02:54 +0100

    finish

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
-    data() {
-        return {
-            userEmail: [],
-        }
-    },
-    methods: {
-        generateEmail(){
-
-        },
-    },
-    mounted() {
-        //generate email and put it into a variable
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            for (let i = 0; i < 10; i++) {
-                this.userEmail[i] = response.data.response;
-                console.log(this.userEmail); 
-            }
-        });
+  data() {
+    return {
+      userEmail: [],
+    };
+  },
+  methods: {
+  },
+  mounted() {
+    //generate email and put it into a variable
+    for (let i = 0; i < 10; i++) {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => {
+        console.log(response.data);
+        this.userEmail[i] = response.data.response;
+      });
+    
     }
-}).mount('#app')
+  },
+}).mount("#app");
